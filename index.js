34694fb0cd11c8c74853230ba91db8bb3ca4bfb1
samar-alazzam <samarazzam354@yahoo.com>
2022-06-02 15:14:59 +0300

    add

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-`use strict`;
+'use strict';
 require ('dotenv').config();
 const server = require('./src/server');
 let PORT = process.env.PORT || 3000;
---
+++ middlewares/getAgent.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function getAgent(req , res , next){
+    req.myName= 'samar';
+    req.browser= req.headers['user-agent'];
+
+
+    next();
+}
+module.exports= getAgent;
